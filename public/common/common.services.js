c1925e2810e67a7f21231347a8878c6aea2c5f75
Lauren Farr <laurenfarr@Lauren-MacBook-Pro.local>
2015-12-10 09:59:56 -0500

    changes

--- public/common/common.services.js
+++ public/common/common.services.js
@@ -1,13 +1,9 @@
 (function() {
   'use strict';
   angular
-<<<<<<< HEAD
+
     .module('bookit');
 
-=======
-    .module('bookit')
-    
->>>>>>> 6cb74b0c82951a7d7c8a887de7dce1c908cd4497
 
 
 }());
