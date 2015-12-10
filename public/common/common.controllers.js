bbaf5fdba01e48fafea02d641d5ab2a9dec91b8c
Grady Rogers <gradyrogers@Gradys-MacBook-Pro.local>
2015-12-10 09:47:46 -0500

    getting logout route

--- public/common/common.controllers.js
+++ public/common/common.controllers.js
@@ -2,7 +2,18 @@
   'use strict';
   angular
     .module('bookit')
-    .controller('MainController', function() {
+    .controller('MainController', function(MainService, $state, $scope) {
+      var vm = this;
+
+      var checkUser = function() {
+        MainService.check().success(function(user) {
+          console.log("Logged in user: ", user);
+        }).error(function() {
+          $state.go('login');
+        });
+      };
+
+        checkUser();
 
     });
 
--- public/common/common.services.js
+++ public/common/common.services.js
@@ -2,6 +2,19 @@
   'use strict';
   angular
     .module('bookit')
+    .factory('MainService', function($http, _) {
+
+      var check = function() {
+        var url = '/get-user';
+        return $http.get(url);
+      };
+
+      return {
+        check: check
+      };
+    })
+
+
 
 
 
