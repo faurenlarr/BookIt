b0171dd48f2b3f87907d83c7eccd9757598d6d83
Grady Rogers <gradyrogers@Gradys-MacBook-Pro.local>
2015-12-15 15:48:38 -0500

    search venues by location

--- public/events/events.controllers.js
+++ public/events/events.controllers.js
@@ -3,17 +3,39 @@
   angular
   .module('events')
     .controller('EventsController', function(
+      $scope,
       $state,
       $stateParams,
       EventsService) {
 
         var vm = this;
 
-        var tester = function() {
-          console.log("EventsController");
+        vm.showForm = true;
+
+        vm.search = function(location) {
+          var param = location.city + ',' + location.state;
+          EventsService.getVenues(param).success(function(venues) {
+            _.each(venues, function(currVal,idx,arr) {
+              if (currVal.description === "") {
+                currVal.description = "no description available";
+              }
+            });
+            vm.venues = venues;
+            console.log(venues);
+            vm.showForm = false;
+            vm.nothaCity = true;
+          });
         };
 
-        tester();
+        vm.newCity = function() {
+          vm.showForm = true;
+          vm.nothaCity = false;
+          vm.venues = [];
+        };
+
+        vm.showDetails = function() {
+          vm.details = true;
+        };
 
     });
 }());
