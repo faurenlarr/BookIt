3483c96eb679658b364a4e9eef1b164d7582b9a1
Lauren Farr <laurenfarr@Lauren-MacBook-Pro.local>
2015-12-15 11:35:47 -0500

    merge and tour show html pages updated

--- public/events/events.module.js
+++ public/events/events.module.js
@@ -6,16 +6,26 @@
     ])
     .config(function($stateProvider) {
       $stateProvider
-      .state('main.book', {
-        url: '/book',
+      .state('main.show', {
+        url: '/show',
         views: {
           'chewy': {
-            templateUrl: 'events/views/searchForm.html',
+            templateUrl: 'events/views/show.html',
             controller: 'EventsController as EController'
           }
         }
       })
-    })
+      .state('main.tour', {
+        url: '/tour',
+        views: {
+          'chewy': {
+            templateUrl: 'events/views/tour.html',
+            controller: 'EventsController as EController'
+          }
+        }
+      });
+    });
+
 
 
 }());
