509c80a39c279312819c771a5fc6248481175977
Grady Rogers <gradyrogers@Gradys-MacBook-Pro.local>
2015-12-19 18:00:24 -0500

    a little button styling and working on saving events

--- public/events/events.controllers.js
+++ public/events/events.controllers.js
@@ -8,23 +8,19 @@
       $stateParams,
       EventsService) {
 
-        $scope.setPaywall = function(item){
-          console.log(item);
-          $scope.paywall = item;
-        };
-        $scope.isPaywall = function(item){
-          console.log(item);
-          if(item === $scope.paywall){
-            return true;
-          } else{
-            return false;
-          }
-        };
-
-
         var vm = this;
 
         vm.showForm = true;
+
+        vm.newCity = function() {
+          vm.showForm = true;
+          vm.nothaCity = false;
+          vm.venues = [];
+        };
+
+        vm.showDetails = function() {
+          vm.details = true;
+        };
 
         vm.search = function(location) {
           var param = location.city + ',' + location.state;
@@ -50,128 +46,10 @@
 
         };
 
-        vm.newCity = function() {
-          vm.showForm = true;
-          vm.nothaCity = false;
-          vm.venues = [];
-        };
-
-        vm.showDetails = function() {
-          vm.details = true;
-        };
-        var formatMonth = function(shows, days) {
-          // time stamp to check for the current day
-          var now = new Date();
-          console.log("now: ",now);
-          // assign shows to days that match the date
-          for (var i = 0; i < shows.length; i++) {
-            for (var j in days) {
-              // check if the day has a show
-              if (days[j].standard.includes(shows[i].start.date)) {
-                days[j].show = shows[i];
-                days[j].available = false;
-              } else if (days[j].long <= now.getDate()) {
-                days[j].available = false;
-              }
-              // assign an id to the first of the month
-              // to shift the block over to the right
-              if (days[j].short === '1st') {
-                if (days[j].ofWeek === 'Sunday') {
-                  days[j].DOMid = 'Sunday';
-                } else if (days[j].ofWeek === 'Monday') {
-                  days[j].DOMid = 'Monday'
-                } else if (days[j].ofWeek === 'Tuesday') {
-                  days[j].DOMid = 'Tuesday'
-                } else if (days[j].ofWeek === 'Wednesday') {
-                  days[j].DOMid = 'Wednesday'
-                } else if (days[j].ofWeek === 'Thursday') {
-                  days[j].DOMid = 'Thursday'
-                } else if (days[j].ofWeek === 'Friday') {
-                  days[j].DOMid = 'Friday'
-                } else if (days[j].ofWeek === 'Saturday') {
-                  days[j].DOMid = 'Saturday'
-                }
-              }
-            } //end of j loop
-          } // end of i loop
-        };
-        var currMonth = function () {
-          var date = new Date();
-          var year = date.getFullYear();
-          var month = date.getMonth();
-          var date = new Date(year, month, 1);
-          var days = [];
-          while (date.getMonth() === month) {
-            var day = {};
-            day.tempId = Math.floor(Math.random() * (100000 - 1) + 1);
-            day.long = new Date(date);
-            day.med = moment(day.long).format('MMM Do');
-            day.short = moment(day.long).format('Do');
-            day.day = moment(day.long).format('dddd');
-            day.ofWeek = moment(day.long).format('dddd');
-            day.month = moment(day.long).format('MMMM');
-            day.monthNum = moment(day.long).format('M');
-            day.standard = moment(day.long).format();
-            days.push(day);
-            date.setDate(date.getDate() + 1);
-          }
-          var id = $stateParams.venueId;
-          EventsService.getCalendar(id).success(function(shows){
-            vm.shows = shows;
-            formatMonth(shows, days);
-          });
-            vm.month = days[0].month;
-            vm.venueName = $stateParams.venueName;
-            vm.days = days;
-            console.log(days);
-        };
-
-        currMonth();
-        vm.nexMonthShows = [];
-
-        vm.nextMonth = function() {
-          console.log("allshows: ", vm.shows);
-          var date = new Date();
-          var month = moment(date).format('M');
-            var nextMonth = month + 1;
-            if (nextMonth > 12) {
-              nextMonth = 1;
-            }
-          console.log('next month: ', nextMonth);
-          for (var i in vm.shows) {
-            console.log('m: ',m);
-            var m = moment(vm.shows[i].start.datetime).format('M');
-            if (m === nextMonth) {
-              nextMonthShows.push(vm.shows[i]);
-            }
-          }
-          console.log('next month shows: ', nextMonthShows);
-          // console.log(month);
-          // console.log('shows: ',vm.shows);
-          // var year = date.getFullYear();
-          // var month = date.getMonth();
-          // var date = new Date(year, month, 1);
-          // var days = [];
-          // while (date.getMonth() === month) {
-          //   var day = {};
-          //   day.long = new Date(date);
-          //   day.med = moment(day.long).format('MMM Do');
-          //   day.short = moment(day.long).format('Do');
-          //   day.day = moment(day.long).format('dddd');
-          //   day.ofWeek = moment(day.long).format('dddd');
-          //   day.month = moment(day.long).format('MMMM');
-          //   day.monthNum = moment(day.long).format('M');
-          //   day.standard = moment(day.long).format();
-          //   days.push(day);
-          //   date.setDate(date.getDate() + 1);
-          // }
-        };
-
 
 
     });
 
-    // $scope.paywall = 1;
 
 
 }());
