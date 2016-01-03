be96e3a697eb10db7ec79b2470492fe5e840a517
alhanger <hanger129@gmail.com>
2016-01-03 14:32:34 -0500

    Rebuild JAR file

--- src/main/java/com/theironyard/services/EventRepository.java
+++ src/main/java/com/theironyard/services/EventRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.CrudRepository;
  * Created by alhanger on 12/8/15.
  */
 public interface EventRepository extends CrudRepository<Event, Integer> {
-    Event findFirstByDate(String date);
     Event findFirstByDateAndVenueName(String date, String venueName);
+    Event findFirstByDateAndBandId(String date, int bandId);
 }
